5f52a43a4c55a992aaca30b65a1d47e0866e1240
akshattyagi <70041605+akshattyagi@users.noreply.github.com>
2020-09-13 21:34:15 +0530

    Profile

---
+++ app/src/main/java/com/example/tasty/activity/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tasty.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tasty.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
